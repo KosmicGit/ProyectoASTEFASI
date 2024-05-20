5de60b87e36b84a5ff286fc7db1ad436f09a2ef4
Siko <kayserhack@gmail.com>
2024-05-20 21:16:53 +0200

    Init Commit

--- src/main/kotlin/Datos/Usuario.kt
+++
@@ -1,9 +0,0 @@
-package es.cifpvirgen.Data
-
-
-data class Usuario(var idUsuario: Int, var username: String, var email: String, var password: String, var rol: Roles, var verificado: Boolean) {
-
-    fun copy(): Usuario {
-        return Usuario(this.idUsuario, this.username, this.email, this.password, this.rol, this.verificado)
-    }
-}
